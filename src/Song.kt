28c66ea297b6d389d1ba1d43aa48e174d453247b
Daksh Sharma <0shard85@solent.ac.uk>
2025-10-29 16:08:25 +0000

    Testing new functionality

--- src/Song.kt
+++ src/Song.kt
@@ -1,1 +1,5 @@
-data class Song(val title: String, val artist: String, val playingTime: Double)
+class Song(val title: String, val artist: String, val playingTime: Double){
+    override fun toString(): String {
+        return "Title: $title, Artist: $artist, Playing Time: $playingTime"
+    }
+}
--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,9 @@
 fun main() {
+    val songs = listOf<Song>(Song("Teddy Picker", "Arctic Monkeys", 2.15), Song("Call it fate, call it karma", "The Strokes",3.50), Song("The winner takes it all", "ABBA", 4.00))
+    val titles = listOf<Music>(Album(songs), Single(songs[0], songs[1]))
 
+    titles.forEach { it ->
+        println(it.getPlayingTime())
+        println(it.getAllSongs().forEach{println(it.toString()) })
+    }
 }
