7b90551324ec48430001d6b6eb4dd4a166c26043
Daksh Sharma <0shard85@solent.ac.uk>
2025-10-29 15:55:16 +0000

    Adding in music interface

--- src/Album.kt
+++ src/Album.kt
@@ -1,6 +1,16 @@
-class Album(val songs: List<Song>) {
+class Album(val songs: List<Song>): Music {
 
     fun filterSongsByArtist(artist: String): List<Song> {
         return songs.filter { it.artist == artist }
+    }
+
+    override fun getAllSongs(): List<Song> {
+        return songs
+    }
+
+    override fun getPlayingTime(): Double {
+        var totalTime = 0.0
+        songs.forEach{totalTime += it.playingTime}
+        return totalTime
     }
 }
